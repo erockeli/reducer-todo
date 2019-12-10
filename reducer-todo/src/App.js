1c53ef4e78d502872cf11e00bccd57214bce5a5c
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-12-10 16:16:00 -0600

    Passing in my functions as props

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const addTodo = item => {
+    dispatch({type:"ADD_TODO", payload: item })
+  }
+  const toggleTodo = id => {dispatch ({type: "TOGGLE_TODO", payload: id})
+};
 
+const deleteCompleted = () => {
+  dispatch({ type: "DELETE_COMPLETED"})
+};
 
   return (
     <div className="App">
-      <TodoForm/>
-  
-      <TodoList/>
-
-      
+      <TodoForm addTodo={addTodo} deleteCompleted={deleteCompleted}/>
+      <TodoList todoList= {state.todoList} toggleTodo={toggleTodo}/>   
     </div>
   );
 }
